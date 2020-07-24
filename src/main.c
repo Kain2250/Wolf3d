10ba7d4befe3a82ea10875edc202b432e1adcf81
Kain2250 <Kain2250@gmail.com>
2020-07-24 11:17:35 +0300

    Add_timer_fix_move

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: bdrinkin <bdrinkin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/21 19:19:04 by kain2250          #+#    #+#             */
-/*   Updated: 2020/07/20 17:16:08 by bdrinkin         ###   ########.fr       */
+/*   Updated: 2020/07/23 19:46:40 by bdrinkin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,21 @@ int			main(int ac, char **av)
 	{
 		if (!(wolf = (t_wolf *)ft_memalloc(sizeof(t_wolf))))
 			return (put_error_sys(ERR_MALLOC));
+		if (!(wolf->time = (t_timer *)ft_memalloc(sizeof(t_timer))))
+			return (put_error_sys(ERR_MALLOC));
 		if (initialization(wolf, av[ac - 1]) == true)
 		{
+			timer_start(wolf->time);
+			filling_var(wolf);
+			raycasting(wolf);
+			SDL_RenderPresent(wolf->sdl.render);
 			while(wolf->quit == false)
 			{
 				if (event_list(wolf) == false)
 					break ;
+				SDL_GetRenderDrawColor(wolf->sdl.render, 0, 0, 0, 0);
+				SDL_RenderClear(wolf->sdl.render);
+
 			}
 		}
 		else
