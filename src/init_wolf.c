e7c55414d457709d85a3a75c2cd096ad71cd39cc
Bean Drinking <bdrinkin@il-g1.msk.21-school.ru>
2020-07-24 20:34:44 +0300

    norm

--- src/init_wolf.c
+++ src/init_wolf.c
@@ -6,15 +6,15 @@
 /*   By: bdrinkin <bdrinkin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/12 19:39:41 by bdrinkin          #+#    #+#             */
-/*   Updated: 2020/07/23 15:52:08 by bdrinkin         ###   ########.fr       */
+/*   Updated: 2020/07/24 19:46:32 by bdrinkin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf3d.h"
 
-void		quit_sdl(t_wolf *wolf)
+void	quit_sdl(t_wolf *wolf)
 {
-	int		i;
+	int	i;
 
 	i = 0;
 	if (wolf->sdl.render != NULL)
@@ -37,9 +37,31 @@ void		quit_sdl(t_wolf *wolf)
 	free(wolf);
 }
 
-bool			load_files(SDL_Texture **textures, SDL_Renderer *render)
+void	filling_var(t_wolf *wolf)
 {
-	int			i;
+	wolf->player.pos_x = 8;
+	wolf->player.pos_y = 12;
+	wolf->player.dir_x = -1;
+	wolf->player.dir_y = 0;
+	wolf->player.plane_x = 0;
+	wolf->player.plane_y = 0.66;
+	wolf->mouse.move_speed = 0.2;
+	wolf->mouse.rot_speed = 0.09;
+	wolf->location.flor = (SDL_Rect *)ft_memalloc(sizeof(SDL_Rect));
+	wolf->location.sky = (SDL_Rect *)ft_memalloc(sizeof(SDL_Rect));
+	wolf->location.sky->h = 32;
+	wolf->location.sky->w = 32;
+	wolf->location.sky->x = 0;
+	wolf->location.sky->y = 0;
+	wolf->location.flor->h = HEIGHT_WIN / 2;
+	wolf->location.flor->w = WIDTH_WIN;
+	wolf->location.flor->x = 0;
+	wolf->location.flor->y = HEIGHT_WIN / 2;
+}
+
+bool	load_files(SDL_Texture **textures, SDL_Renderer *render)
+{
+	int	i;
 
 	textures[texture_main_menu] = IMG_LoadTexture(render, TEX_MENU);
 	textures[texture_concrete] = IMG_LoadTexture(render, TEX_CONCRETE);
@@ -59,13 +81,13 @@ bool			load_files(SDL_Texture **textures, SDL_Renderer *render)
 	return (true);
 }
 
-bool		init_sdl(t_wolf *wolf)
+bool	init_sdl(t_wolf *wolf)
 {
 	if (SDL_Init(SDL_INIT_EVERYTHING) == -1)
 		return (put_error_sdl(ERR_INIT_SDL, SDL_GetError()));
 	if (TTF_Init() == -1)
 		return (put_error_sdl(ERR_INIT_TTF, SDL_GetError()));
-	if(!(IMG_Init(IMG_INIT_JPG) & IMG_INIT_JPG))
+	if (!(IMG_Init(IMG_INIT_JPG) & IMG_INIT_JPG))
 		return (put_error_sdl(ERR_INIT_IMG, IMG_GetError()));
 	if ((wolf->sdl.window = SDL_CreateWindow(NAME_WIN, SDL_WINDOWPOS_UNDEFINED,
 		SDL_WINDOWPOS_UNDEFINED, WIDTH_WIN,
@@ -77,8 +99,13 @@ bool		init_sdl(t_wolf *wolf)
 	return (true);
 }
 
-bool		initialization(t_wolf *wolf, char *map)
+bool	initialization(t_wolf *wolf, char *map)
 {
+	if (!(wolf->time = (t_timer *)ft_memalloc(sizeof(t_timer))))
+	{
+		put_error_sys(ERR_MALLOC);
+		return (false);
+	}
 	if (init_sdl(wolf) == false)
 		return (false);
 	else if (pars_map(wolf, map) == 1)
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: bdrinkin <bdrinkin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/21 19:19:04 by kain2250          #+#    #+#             */
-/*   Updated: 2020/07/23 19:46:40 by bdrinkin         ###   ########.fr       */
+/*   Updated: 2020/07/24 19:03:56 by bdrinkin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,15 @@ int			main(int ac, char **av)
 	{
 		if (!(wolf = (t_wolf *)ft_memalloc(sizeof(t_wolf))))
 			return (put_error_sys(ERR_MALLOC));
-		if (!(wolf->time = (t_timer *)ft_memalloc(sizeof(t_timer))))
-			return (put_error_sys(ERR_MALLOC));
 		if (initialization(wolf, av[ac - 1]) == true)
 		{
 			timer_start(wolf->time);
 			filling_var(wolf);
 			raycasting(wolf);
-			SDL_RenderPresent(wolf->sdl.render);
-			while(wolf->quit == false)
+			while (wolf->quit == false)
 			{
 				if (event_list(wolf) == false)
 					break ;
-				SDL_GetRenderDrawColor(wolf->sdl.render, 0, 0, 0, 0);
-				SDL_RenderClear(wolf->sdl.render);
-
 			}
 		}
 		else
@@ -46,12 +40,3 @@ int			main(int ac, char **av)
 		ft_putendl(USAGE_W3D);
 	return (0);
 }
-
-// Захват мыши в окне
-// if (SDL_CaptureMouse(SDL_TRUE) == -1)
-// {
-// 	put_error_sdl(ERR_CAPTURE_MOUSE, SDL_GetError());
-// 	return (1);
-// }
-// else
-// 	return (0);
