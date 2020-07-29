936fe8fadb2e3e330f8a349bbf5db817ea6856b9
Bean Drinking <bdrinkin@il-h3.msk.21-school.ru>
2020-07-29 17:44:47 +0300

    fix_SDL2Lib_compile

---
+++ configure.sh
@@ -0,0 +1,30 @@
+cd allLibs
+mkdir -p temp_src
+git clone https://github.com/davidsiaw/SDL2.git temp_src/SDL2-2
+./temp_src/SDL2-2/configure --prefix $(PWD)/SDL2_lib/
+make
+make install
+make clean
+make distclean
+rm -rf temp_src/SDL2-2
+git clone https://github.com/davidsiaw/SDL2_image.git temp_src/SDL2_image
+./temp_src/SDL2_image/configure --prefix $(PWD)/SDL2_lib/
+make
+make install
+make clean
+make distclean
+rm -rf temp_src/SDL2_image
+git clone https://github.com/davidsiaw/SDL2_mixer.git temp_src/SDL2_mixer
+./temp_src/SDL2_mixer/configure --prefix $(PWD)/SDL2_lib/
+make
+make install
+make clean
+make distclean
+rm -rf temp_src/SDL2_mixer
+git clone https://github.com/davidsiaw/SDL2_ttf.git temp_src/SDL2_ttf
+./temp_src/SDL2_ttf/configure --prefix $(PWD)/SDL2_lib/
+make
+make install
+make clean
+make distclean
+rm -rf temp_src/SDL2_ttf
