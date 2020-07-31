e7d50b6986878cf1235f197d711efc8a82d05114
Bean Drinking <bdrinkin@il-f1.msk.21-school.ru>
2020-07-31 22:48:09 +0300

    clean_trash

--- configure.sh
+++ configure.sh
@@ -6,28 +6,20 @@ git clone https://github.com/davidsiaw/SDL2.git temp_src/SDL2-2
 make
 make install
 make clean
-#make distclean
+make distclean
 rm -rf temp_src/SDL2-2
 git clone https://github.com/davidsiaw/SDL2_image.git temp_src/SDL2_image
 ./temp_src/SDL2_image/configure --prefix $(PWD)/SDL2_lib/
 make
 make install
 make clean
-# make distclean
+make distclean
 rm -rf temp_src/SDL2_image
 git clone https://github.com/davidsiaw/SDL2_mixer.git temp_src/SDL2_mixer
 ./temp_src/SDL2_mixer/configure --prefix $(PWD)/SDL2_lib/
 make
 make install
 make clean
-# make distclean
-rm -rf temp_src/SDL2_mixer
-brew install freetype
-git clone https://github.com/davidsiaw/SDL2_ttf.git temp_src/SDL2_ttf
-./temp_src/SDL2_ttf/configure --prefix $(PWD)/SDL2_lib/
-make
-make install
-make clean
 make distclean
-rm -rf temp_src/SDL2_ttf
+rm -rf temp_src/SDL2_mixer
  
