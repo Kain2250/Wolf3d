9327c2ef59b345737634515ed0eee8df09905d67
Bean Drinking <bdrinkin@il-f1.msk.21-school.ru>
2020-07-31 23:15:24 +0300

    make_fix + config_fix

--- configure.sh
+++ configure.sh
@@ -6,20 +6,17 @@ git clone https://github.com/davidsiaw/SDL2.git temp_src/SDL2-2
 make
 make install
 make clean
-make distclean
 rm -rf temp_src/SDL2-2
 git clone https://github.com/davidsiaw/SDL2_image.git temp_src/SDL2_image
 ./temp_src/SDL2_image/configure --prefix $(PWD)/SDL2_lib/
 make
 make install
 make clean
-make distclean
 rm -rf temp_src/SDL2_image
 git clone https://github.com/davidsiaw/SDL2_mixer.git temp_src/SDL2_mixer
 ./temp_src/SDL2_mixer/configure --prefix $(PWD)/SDL2_lib/
 make
 make install
 make clean
-make distclean
 rm -rf temp_src/SDL2_mixer
  
