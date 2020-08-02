0f3c82ca101380c34bef5b9b2d63d1f6c2b0749f
Bean Drinking <bdrinkin@oa-l5.msk.21-school.ru>
2020-08-02 21:01:38 +0300

    add_wall_texture

--- src/mouse_set.c
+++ src/mouse_set.c
@@ -6,8 +6,45 @@
 /*   By: bdrinkin <bdrinkin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/30 19:07:45 by bdrinkin          #+#    #+#             */
-/*   Updated: 2020/08/01 17:57:42 by bdrinkin         ###   ########.fr       */
+/*   Updated: 2020/08/02 20:17:35 by bdrinkin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf3d.h"
+
+	// if (wolf->location.map[wolf->player.map_x][wolf->player.map_y] == '1')
+	// {
+	// 	if (wolf->player.side == 1)
+	// 	{
+	// 		SDL_GetTextureColorMod(wolf->sdl.textures[texture_wood_box], &walls->color->r, &walls->color->g, &walls->color->b);
+	// 		SDL_SetTextureColorMod(wolf->sdl.textures[texture_wood_box], walls->color->r / 2, walls->color->g / 2, walls->color->b / 2);
+	// 	}
+	// 	SDL_RenderCopy(wolf->sdl.render, wolf->sdl.textures[texture_wood_box], &rect_wall, &rect_wall);
+	// }
+	// else if (wolf->location.map[wolf->player.map_x][wolf->player.map_y] == '2')
+	// {
+	// 	if (wolf->player.side == 1)
+	// 	{
+	// 		SDL_GetTextureColorMod(wolf->sdl.textures[texture_wood_door], &walls->color->r, &walls->color->g, &walls->color->b);
+	// 		SDL_SetTextureColorMod(wolf->sdl.textures[texture_wood_door], walls->color->r / 2, walls->color->g / 2, walls->color->b / 2);
+	// 	}
+	// 	SDL_RenderCopy(wolf->sdl.render, wolf->sdl.textures[texture_wood_door], &rect, &rect_wall);
+	// }
+	// else if (wolf->location.map[wolf->player.map_x][wolf->player.map_y] == '3')
+	// {
+	// 	if (wolf->player.side == 1)
+	// 	{
+	// 		SDL_GetTextureColorMod(wolf->sdl.textures[texture_sand], &walls->color->r, &walls->color->g, &walls->color->b);
+	// 		SDL_SetTextureColorMod(wolf->sdl.textures[texture_sand], walls->color->r / 2, walls->color->g / 2, walls->color->b / 2);
+	// 	}
+	// 	SDL_RenderCopy(wolf->sdl.render, wolf->sdl.textures[texture_sand], &rect, &rect_wall);
+	// }
+	// else if (wolf->location.map[wolf->player.map_x][wolf->player.map_y] == '4')
+	// {
+	// 	if (wolf->player.side == 1)
+	// 	{
+	// 		SDL_GetTextureColorMod(wolf->sdl.textures[texture_concrete], &walls->color->r, &walls->color->g, &walls->color->b);
+	// 		SDL_SetTextureColorMod(wolf->sdl.textures[texture_concrete], walls->color->r / 2, walls->color->g / 2, walls->color->b / 2);
+	// 	}
+	// 	SDL_RenderCopy(wolf->sdl.render, wolf->sdl.textures[texture_concrete], &rect, &rect_wall);
+	// }
