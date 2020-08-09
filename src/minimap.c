186b93aa935dfa936e82b9ad163b1b6c7d888f9e
Bean Drinking <bdrinkin@il-h5.msk.21-school.ru>
2020-08-09 20:59:38 +0300

    m

--- src/minimap.c
+++ src/minimap.c
@@ -6,7 +6,7 @@
 /*   By: bdrinkin <bdrinkin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/09 17:53:36 by bdrinkin          #+#    #+#             */
-/*   Updated: 2020/08/09 20:45:26 by bdrinkin         ###   ########.fr       */
+/*   Updated: 2020/08/09 20:59:18 by bdrinkin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,14 @@ static void	put_player(t_wolf *wolf)
 {
 	SDL_Rect	rect;
 
-	rect.x = wolf->player.pos_y * 15;
-	rect.y = wolf->player.pos_x * 15;
+	rect.x = wolf->player.pos_y * 10 + 10;
+	rect.y = wolf->player.pos_x * 10 + 10;
 	rect.h = 4;
 	rect.w = 4;
 	SDL_SetRenderDrawColor(wolf->sdl.render, 255, 0, 0, 0);
 	SDL_RenderFillRect(wolf->sdl.render, &rect);
-	SDL_RenderDrawLineF(wolf->sdl.render, wolf->player.dir_y * 40 + rect.x, wolf->player.dir_x * 40 + rect.y, rect.x, rect.y);
+	SDL_RenderDrawLineF(wolf->sdl.render, wolf->player.dir_y * 40 + rect.x,
+	wolf->player.dir_x * 40 + rect.y, rect.x + 2, rect.y + 2);
 }
 
 void		minimap_output(t_wolf *wolf)
@@ -47,6 +48,7 @@ void		minimap_output(t_wolf *wolf)
 			wolf->location.map[y][x] == '0' ?
 			SDL_RenderDrawRect(wolf->sdl.render, &rect) :
 			SDL_RenderFillRect(wolf->sdl.render, &rect);
+			 
 			rect.x += 10;
 			x++;
 			put_player(wolf);
@@ -54,4 +56,5 @@ void		minimap_output(t_wolf *wolf)
 		rect.y += 10;
 		y++;
 	}
+
 }
